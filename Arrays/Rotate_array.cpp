70bedf0ed997650dc737c0573e77d67d57a93f45
karanloona <karanloona77@gmail.com>
2021-07-21 16:27:50 +0530

    Rotate array

---
+++ Arrays/Rotate_array.cpp
@@ -0,0 +1,27 @@
+/*
+
+	Given an array with N elements, the task is to rotate the array to the left by K steps, where K is non-negative.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+	int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int k;
+    cin>>k;
+   	for(int i=k; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    for(int i=0; i<k; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
