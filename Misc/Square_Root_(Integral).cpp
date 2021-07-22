89a1d2ce366c5960b3e2dfdc16ca07502d86e50e
karanloona <karanloona77@gmail.com>
2021-07-22 12:34:46 +0530

    Square Root (Integral)

---
+++ Misc/Square_Root_(Integral).cpp
@@ -0,0 +1,17 @@
+/*
+
+	Given a number N, find its square root. You need to find and print only the integral part of square root of N.
+	For eg. if number given is 18, answer is 4.
+
+*/
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; 
+   	cin>>n;
+    int ans=sqrt(n);
+    cout<<ans;
+}
