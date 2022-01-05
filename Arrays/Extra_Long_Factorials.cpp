ee515a94eae43f7c55094902d228d75dc5b86ad7
karanloona <karan.loona@ecosystm360.com>
2022-01-05 22:04:02 +0530

    Extra Long Factorial

---
+++ Arrays/Extra_Long_Factorials.cpp
@@ -0,0 +1,23 @@
+// The factorial of the integer n, written n!, is defined as:
+
+// n!=n*n(n-1)*(n-2)*.......*3*2*1
+
+// Calculate and print the factorial of a given integer.
+
+// For example, if n=30, we calculate  and get 265252859812191058636308480000000.
+
+// Function Description
+
+// Complete the extraLongFactorials function in the editor below. It should print the result and return.
+
+// extraLongFactorials has the following parameter(s):
+
+// n: an integer
+// Note: Factorials of  can't be stored even in a  long long variable. Big integers must be used for such calculations. Languages like Java, Python, Ruby etc. can handle big integers, but we need to write additional code in C/C++ to handle huge values.
+
+// We recommend solving this challenge using BigIntegers.
+
+
+void extraLongFactorials(int n) {
+    
+}
